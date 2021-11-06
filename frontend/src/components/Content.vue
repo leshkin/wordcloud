2daca31bbee7a5f6b79b4fd18ee3da3651e665b8
leshkin <alaposhin@gmail.com>
2021-11-06 14:29:15 +0300

    update responsiveness

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -105,50 +105,52 @@
   <nav class="navbar">
     <div class="navbar-brand">
       <a class="navbar-item is-size-1 has-text-weight-bold" href="https://wordcloud.online">
-        <span class="has-text-link ">Word</span> <span class="has-text-danger">Cloud</span>
+        <span class="has-text-link">Word</span> <span class="has-text-danger">Cloud</span>
       </a>
     </div>
   </nav>
-  <section class="hero is-fullheight-with-navbar">
-    <div class="columns is-mobile is-centered">
-      <div class="column is-two-thirds">
-      <article v-if="isError" class="message is-danger">
-        <div class="message-body">{{ t('errors.textTooLong') }}</div>
-      </article>
-      <article class="message is-dark">
-        <div class="message-body">{{ t('info') }}</div>
-      </article>
-      <div class="field">
-        <div class="control">
-          <textarea class="textarea" v-model="text" :placeholder="t('placeholder')" rows="10"></textarea>
-        </div>
-      </div>
-      <div class="field has-text-centered">
-        <div class="control">
-          <button class="button is-link" :class="{'is-loading': isLoading}" @click="generate()">{{ t('generate') }}</button>
-        </div>
-      </div>
-      <client-only>
-        <word-cloud :words="words"></word-cloud>
-        <canvas id="canvas" class="is-hidden" width="500" height="500"></canvas>
-      </client-only>
-      <div class="field has-text-centered">
-        <div class="control">
-          <button v-if="isCreated" class="button is-success" @click="downloadPNG()">{{ t('downloadPNG') }}</button>
+  <section>
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <div class="container m-2">
+          <article v-if="isError" class="message is-danger">
+            <div class="message-body">{{ t('errors.textTooLong') }}</div>
+          </article>
+          <article class="message is-black">
+            <div class="message-body">{{ t('info') }}</div>
+          </article>
+          <div class="field">
+            <div class="control">
+              <textarea class="textarea" v-model="text" :placeholder="t('placeholder')" rows="10"></textarea>
+            </div>
+          </div>
+          <div class="field has-text-centered">
+            <div class="control">
+              <button class="button is-link" :class="{'is-loading': isLoading}" @click="generate()">{{ t('generate') }}</button>
+            </div>
+          </div>
+          <client-only>
+            <word-cloud :words="words"></word-cloud>
+            <canvas id="canvas" class="is-hidden" width="500" height="500"></canvas>
+          </client-only>
+          <div class="field has-text-centered">
+            <div class="control">
+              <button v-if="isCreated" class="button is-success" @click="downloadPNG()">{{ t('downloadPNG') }}</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </section>
   <footer class="footer mt-3">
     <div class="content has-text-centered">
       <p>
-        <router-link to="/en">English</router-link>
-        <router-link to="/es" class="ml-3">Español</router-link>
-        <router-link to="/pt" class="ml-3">Português</router-link>
-        <router-link to="/fr" class="ml-3">Français</router-link>
-        <router-link to="/de" class="ml-3">Deutsch</router-link>
-        <router-link to="/ru" class="ml-3">Русский</router-link>
+        <router-link to="/en" class="is-inline-block">English</router-link>
+        <router-link to="/es" class="ml-3 is-inline-block">Español</router-link>
+        <router-link to="/pt" class="ml-3 is-inline-block">Português</router-link>
+        <router-link to="/fr" class="ml-3 is-inline-block">Français</router-link>
+        <router-link to="/de" class="ml-3 is-inline-block">Deutsch</router-link>
+        <router-link to="/ru" class="ml-3 is-inline-block">Русский</router-link>
       </p>
       <p>
         Based on <a href="https://github.com/jasondavies/d3-cloud">d3-cloud</a> and <a href="https://github.com/explosion/spaCy">spaCy</a> libraries
