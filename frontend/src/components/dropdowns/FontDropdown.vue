9325a00f6a51ab4d1bfbb77d989b78bf3613890d
leshkin <alaposhin@gmail.com>
2022-03-16 17:07:23 +0300

    feat: show only supported fonts

--- frontend/src/components/dropdowns/FontDropdown.vue
+++ frontend/src/components/dropdowns/FontDropdown.vue
@@ -1,50 +1,62 @@
 <script setup>
-  import { ref } from 'vue'
-  import { onClickOutside } from '@vueuse/core'
-  import { FONTS } from '/src/config.js'
+import { ref, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { onClickOutside } from '@vueuse/core'
+import { FONTS } from '/src/config.js'
 
-  let isActive = ref(false)
-  let currentFont = ref(FONTS[0])
-  const props = defineProps({ modelValue: Object })
-  const emit = defineEmits(['update:modelValue'])
+const { locale } = useI18n({ useScope: 'global' })
+let isActive = ref(false)
+const props = defineProps({ modelValue: Object })
+const emit = defineEmits(['update:modelValue'])
 
-  const dropdown = ref(null)
-  onClickOutside(dropdown, () => {
-      isActive.value = false
-    }
-  )
+const fonts = computed(() => {
+  return FONTS.filter((font) => font.lang.includes(locale.value))
+})
 
-  const open = () => {
-    isActive.value = !isActive.value
-  }
+let currentFont = ref(fonts.value[0])
 
-  const select = (font) => {
-    currentFont.value = font
-    isActive.value = !isActive.value
-    emit('update:modelValue', font)
-  }
+const dropdown = ref(null)
+
+onClickOutside(dropdown, () => {
+  isActive.value = false
+})
+
+const open = () => {
+  isActive.value = !isActive.value
+}
+
+const select = (font) => {
+  currentFont.value = font
+  isActive.value = !isActive.value
+  emit('update:modelValue', font)
+}
 </script>
 
 <template>
-  <div class="dropdown" :class="{'is-active': isActive}" ref="dropdown">
+  <div class="dropdown" :class="{ 'is-active': isActive }" ref="dropdown">
     <div class="dropdown-trigger">
-      <button class="button" aria-haspopup="true" aria-controls="dropdown-menu"
-              @click="open()" :style="{'font-family': currentFont.family}">
+      <button
+        class="button"
+        aria-haspopup="true"
+        aria-controls="dropdown-menu"
+        @click="open()"
+        :style="{ 'font-family': currentFont.family }"
+      >
         <span>{{ currentFont.family }}</span>
         <span class="icon is-small">
-          <span class="material-icons">
-            expand_more
-          </span>
+          <span class="material-icons"> expand_more </span>
         </span>
       </button>
     </div>
     <div class="dropdown-menu" id="dropdown-menu" role="menu">
       <div class="dropdown-content">
-        <div v-for="font in FONTS"
-             :key="font" class="dropdown-item"
-             @click="select(font)"
-             :class="{'has-background-info-light': font.family === currentFont.family}"
-             :style="{'font-family': font.family}">
+        <div
+          v-for="font in fonts"
+          class="dropdown-item"
+          @click="select(font)"
+          :class="{ 'has-background-info-light': font.family === currentFont.family }"
+          :style="{ 'font-family': font.family }"
+        >
           {{ font.family }}
         </div>
       </div>
@@ -53,8 +65,8 @@
 </template>
 
 <style scoped>
-  .dropdown-item {
-    cursor: pointer;
-    font-size: 1rem;
-  }
+.dropdown-item {
+  cursor: pointer;
+  font-size: 1rem;
+}
 </style>
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,61 +1,69 @@
 export const FONTS = [
   {
     family: 'Lobster',
-    lang: ['latin']
+    lang: ['en', 'es', 'de', 'fr', 'pt', 'ru'],
   },
   {
     family: 'Roboto',
-    lang: ['latin'] // todo: add other alphabets
+    lang: ['en', 'es', 'de', 'fr', 'pt', 'ru'],
   },
   {
     family: 'Alfa Slab One',
-    lang: ['latin']
+    lang: ['en', 'es', 'de', 'fr', 'pt'],
+  },
+  {
+    family: 'Russo One',
+    lang: ['en', 'es', 'de', 'fr', 'pt', 'ru'],
   },
   {
     family: 'Bebas Neue',
-    lang: ['latin']
+    lang: ['en', 'es', 'de', 'fr', 'pt'],
   },
   {
     family: 'Source Serif Pro',
-    lang: ['latin']
-  }
+    lang: ['en', 'es', 'de', 'fr', 'pt', 'ru'],
+  },
+  {
+    family: 'Noto Sans SC',
+    lang: ['zh'],
+  },
 ]
 
 export const COLOR_PALETTES = [
   {
     name: 'contrasting-1',
     colors: ['#e76f3d', '#feab6b', '#f3e9e7', '#9bcfe0', '#00a7c7'],
-    backgroundColor: '#000000'
+    backgroundColor: '#000000',
   },
   {
     name: 'blue',
     colors: ['#264653', '#2a9d8f', '#e9c46a', '#f4a261', '#e76f51'],
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   {
     name: 'green',
     colors: ['#386641', '#6a994e', '#a7c957', '#f2e8cf', '#bc4749'],
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   {
     name: 'cold-1',
     colors: ['#252b31', '#5e6668', '#c1c8c7', '#f6fafb', '#d49c6b'],
-    backgroundColor: '#000000'
+    backgroundColor: '#000000',
   },
   {
     name: 'black',
     colors: ['#000000'],
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
   {
     name: 'white',
     colors: ['#ffffff'],
-    backgroundColor: '#000000'
-  }
+    backgroundColor: '#000000',
+  },
 ]
 
 const GOOGLE_FONTS_URL = 'https://fonts.googleapis.com/css2?display=swap'
 
-export function getFontURL (font) {
+export function getFontURL(font) {
   return GOOGLE_FONTS_URL + '&family=' + font.family.replaceAll(' ', '+')
 }
